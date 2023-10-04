3831cc9865e4562fb3e03b44d7d59ab587943a35
iamsixwater <soup.sixwater@gmail.com>
2023-10-04 11:19:09 +1100

    [ADD] logout function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { HiOutlineShoppingBag, HiOutlinePencil } from 'react-icons/hi';
-import { login } from '../api/firebase';
+import { login, logout, onUserStateChanged } from '../api/firebase';
 
 export default function Header() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChanged((user) => {
+      setUser(user);
+    });
+  }, []);
+
+  const handleLogin = () => {
+    login().then(setUser);
+  };
+  const handleLogout = () => {
+    logout().then(setUser);
+  };
+
   return (
     <header className='flex justify-between border-b border-gray-300 p-2'>
       <Link to='/' className='flex items-center text-4xl text-brand'>
@@ -16,7 +31,8 @@ export default function Header() {
         <Link to='/products/add' className='text-2xl'>
           <HiOutlinePencil />
         </Link>
-        <button onClick={login}>Login</button>
+        {!user && <button onClick={handleLogin}>Login</button>}
+        {user && <button onClick={handleLogout}>Logout</button>}
       </nav>
     </header>
   );
