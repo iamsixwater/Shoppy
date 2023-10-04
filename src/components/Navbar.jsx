1ae8456d9683d0b2da70ee79262af1b16eb13e71
iamsixwater <soup.sixwater@gmail.com>
2023-10-04 11:43:27 +1100

    [ADD] user profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { HiOutlineShoppingBag, HiOutlinePencil } from 'react-icons/hi';
 import { login, logout, onUserStateChanged } from '../api/firebase';
+import User from './User';
 
 export default function Header() {
   const [user, setUser] = useState();
 
   useEffect(() => {
-    onUserStateChanged((user) => {
-      setUser(user);
-    });
+    onUserStateChanged(setUser);
   }, []);
-
-  const handleLogin = () => {
-    login().then(setUser);
-  };
-  const handleLogout = () => {
-    logout().then(setUser);
-  };
 
   return (
     <header className='flex justify-between border-b border-gray-300 p-2'>
@@ -31,8 +23,9 @@ export default function Header() {
         <Link to='/products/add' className='text-2xl'>
           <HiOutlinePencil />
         </Link>
-        {!user && <button onClick={handleLogin}>Login</button>}
-        {user && <button onClick={handleLogout}>Logout</button>}
+        {user && <User user={user} />}
+        {!user && <button onClick={login}>Login</button>}
+        {user && <button onClick={logout}>Logout</button>}
       </nav>
     </header>
   );
---
+++ src/components/User.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function User({ user: { photoURL, displayName } }) {
+  return (
+    <div className='flex items-center'>
+      <img
+        className='w-10 h-10 rounded-full'
+        src={photoURL}
+        alt={displayName}
+        referrerPolicy='no-referrer'
+      />
+      <span className='hidden md:block ml-4'>{displayName}</span>
+    </div>
+  );
+}
