d3b47f259deb22662f6ff0faa349e150d89f8361
iamsixwater <soup.sixwater@gmail.com>
2023-10-09 10:38:52 +1100

    [ADD] product detail page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function ProductCard({
-  product: { image, title, price, category },
+  product,
+  product: { id, image, title, price, category },
 }) {
+  const navigate = useNavigate();
   return (
-    <li className='rounded-md shadow-md p-2 cursor-pointer overflow-hidden'>
+    <li
+      className='rounded-md shadow-md p-2 cursor-pointer overflow-hidden hover:scale-105 transition-all'
+      onClick={() => {
+        navigate(`/products/${id}`, { state: { product } });
+      }}
+    >
       <img className='w-full' src={image} alt={title} />
       <div className='px-2 mt-2 text-lg flex justify-between items-center'>
         <h3 className='font-bold truncate'>{title}</h3>
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,5 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import Button from '../components/ui/Button';
 
 export default function ProductDetail() {
-  return <div>product detail</div>;
+  const {
+    state: {
+      product: { id, category, title, description, image, price, options },
+    },
+  } = useLocation();
+  const [selected, setSelected] = useState(options && options[0]);
+  const handleChange = (e) => {
+    setSelected(e.target.value);
+  };
+  const handleClick = () => {};
+
+  return (
+    <>
+      <p className='text-gray-600 mx-12 mt-4'>{category}</p>
+      <section className='flex flex-col md:flex-row p-4'>
+        <img className='x-full basis-7/12 px-4' src={image} alt={title} />
+        <section className='w-full flex flex-col basis-5/12 p-4'>
+          <h1 className='text-3xl font-bold py-2'>{title}</h1>
+          <p className='text-xl font-bold pb-2 text-gray-500 border-b border-gray-400'>
+            ${price}
+          </p>
+          <p className='text-lg py-4'>{description}</p>
+          <div className='flex items-center'>
+            <label className='text-brand' htmlFor='select'>
+              Options:
+            </label>
+            <select
+              className='outline-none border-2 border-dashed border-brand m-4 p-2 flex-1'
+              id='select'
+              onChange={handleChange}
+              value={selected}
+            >
+              {options &&
+                options.map((option, idx) => (
+                  <option key={idx}>{option}</option>
+                ))}
+            </select>
+          </div>
+          <Button text='Add to cart' onClick={handleClick} />
+        </section>
+      </section>
+    </>
+  );
 }
